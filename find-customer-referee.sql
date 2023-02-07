a8ce96c5d10d9e45224f44d23aa2ac5f110a8b7d
SeldomseenSchweig <jamesschweig@gmail.com>
2023-02-07 11:19:03 -0800

    find-customer-reference.sql

---
+++ find-customer-referee.sql
@@ -0,0 +1,13 @@
+-- Runtime
+-- 925 ms
+-- Beats
+-- 32.27%
+-- Click the distribution chart to view more details
+-- Notes
+-- Write your notes here
+-- Related Tags
+-- Select tags
+-- 0/5
+# Write your MySQL query statement below
+SELECT name FROM Customer
+WHERE referee_id IS null OR referee_id != 2
